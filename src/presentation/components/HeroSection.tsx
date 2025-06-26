3f9e058de0ee78be64effddb86b375449b502b61
SrLLamaDev <srllamadev@gmail.com>
2025-06-25 22:45:10 -0400

    update del inicio

--- src/presentation/components/HeroSection.tsx
+++ src/presentation/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import './HeroSection.css';
 
 export const HeroSection = () => (
   <section className="section hero">
-    <h1>Bienvenido a Chess Project</h1>
+    <h1 className='hero-title'>Bienvenido a Chess Project</h1>
     <p>Juega, aprende y mejora tus habilidades en ajedrez.</p>
     <button>Comenzar</button>
   </section>
